7c30091bc567dd5e9fd0b8f8b4857e601a567d6c
feng-tao <fengtao04@gmail.com>
2020-07-03 21:41:48 -0700

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='test-repo',
+    name='tao-test-repo',
     version='0.0.1',
     description='My private package from private github repo',
-    url='git@github.com:feng-tao/test-package-pypi.git',
+    #url='git@github.com:feng-tao/test-package-pypi.git',
     author='Tao Feng',
     author_email='t@gmail.com',
     license='unlicense',
-    packages=['test-repo'],
+    packages=['tao-test-repo'],
     zip_safe=False
 )
