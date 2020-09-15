0e20cc8476c48b371c4e8b82736b05df9c59ce17
feng-tao <fengtao04@gmail.com>
2020-09-14 23:12:30 -0700

    test pr on lark

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 setup(
     name='tao-test-repo',
-    version='0.0.1',
+    version='0.0.2',
     description='My private package from private github repo',
     #url='git@github.com:feng-tao/test-package-pypi.git',
     author='Tao Feng',
     author_email='t@gmail.com',
+    install_requires=[
+        'lark-parser==0.8.5',
+    ],
     license='unlicense',
     packages=['tao-test-repo'],
     zip_safe=False
